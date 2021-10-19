032c835009eaea34b75563ffbb40356c0f9baa64
viniciuszorzanelli <viniciuszorzanelli@wine.com.br>
2021-10-19 08:33:37 -0300

    Initial commit

---
+++ src/main/java/com/zortech/tincar/TincarApplication.java
@@ -0,0 +1,13 @@
+package com.zortech.tincar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TincarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TincarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zortech/tincar/TincarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zortech.tincar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TincarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
